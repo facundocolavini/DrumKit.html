ae1c838da5e770ebb3217656a9d13dac9e5eff28
facundocolavini <54385792+facundocolavini@users.noreply.github.com>
2021-06-02 17:09:25 -0300

    DrumKit

--- drum_script.js
+++ drum_script.js
@@ -1,6 +1,5 @@
 //Evento
-window.addEventListener('keydown',function(e){
-
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if(!audio) return; //stop running audio
@@ -8,15 +7,14 @@ window.addEventListener('keydown',function(e){
     audio.play()
     console.log(key);
     key.classList.add('playing');
- 
-});
-const keys = document.querySelectorAll('.key');
-console.log(keys);  
-
-keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
+}
 
 function removeTransition(e){
-if(e.propertyName !== 'transform')return;
-console.log(e.propertyName)
-this.classList.remove('playing');
+    if(e.propertyName !== 'transform')return;
+    console.log(e.propertyName)
+    this.classList.remove('playing');
 }
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key=>key.addEventListener('transitionend',removeTransition));
+window.addEventListener('keydown',playSound);
